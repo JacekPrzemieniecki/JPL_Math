03d08f8ccba9ede5974da3f01e16f4be9e539f21
JPrzemieniecki <jacek@przemieniecki.net>
2019-08-04 13:28:51 +0200

    Find max/min

--- JPLMath.cs
+++ JPLMath.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
+using Unity.Collections;
 using Unity.Mathematics;
+using UnityEngine.Assertions;
 using static Unity.Mathematics.math;
 
 namespace JPL.Math
 {
-
     public static class JPLMath
     {
         const float SmallFloat = 1e-5f;
+        public static float sqr(float a) => a * a;
+
         public static void minmax(float a, float b, out float min, out float max)
         {
             if (a > b)
@@ -104,6 +107,114 @@ namespace JPL.Math
         {
             var r = rotate(v.xy, angle);
             return float3(r, v.z);
+        }
+
+        public static int indexOfMin<T>(NativeSlice<T> slice) where T: struct, IComparable<T>
+        {
+            Assert.IsTrue(slice.Length > 0);
+            int idx = 0;
+            for (int i = 1; i < slice.Length; i++)
+            {
+                if (slice[idx].CompareTo(slice[i]) > 0)
+                {
+                    idx = i;
+                }
+            }
+            return idx;
+        }
+        public static T findMin<T>(NativeSlice<T> slice) where T : struct, IComparable<T> 
+            => slice[indexOfMin(slice)];
+        public static T findMin<T>(NativeArray<T> arr) where T : struct, IComparable<T>
+            => findMin(arr.Slice());
+
+        public static int indexOfMax<T>(NativeSlice<T> slice) where T : struct, IComparable<T>
+        {
+            Assert.IsTrue(slice.Length > 0);
+            int idx = 0;
+            for (int i = 1; i < slice.Length; i++)
+            {
+                if (slice[idx].CompareTo(slice[i]) < 0)
+                {
+                    idx = i;
+                }
+            }
+            return idx;
+        }
+        public static T findMax<T>(NativeSlice<T> slice) where T : struct, IComparable<T>
+            => slice[indexOfMax(slice)];
+        public static T findMax<T>(NativeArray<T> arr) where T : struct, IComparable<T>
+            => findMax(arr.Slice());
+
+
+        public static float average(NativeSlice<float> slice)
+        {
+            Assert.IsTrue(slice.Length > 0);
+            return sum(slice) / slice.Length;
+        }
+        public static float2 average(NativeSlice<float2> slice)
+        {
+            Assert.IsTrue(slice.Length > 0);
+            return sum(slice) / slice.Length;
+        }
+        public static float3 average(NativeSlice<float3> slice)
+        {
+            Assert.IsTrue(slice.Length > 0);
+            return sum(slice) / slice.Length;
+        }
+
+        public static float sum(NativeSlice<float> slice)
+        {
+            float s = default;
+            for (int i = 0; i < slice.Length; i++)
+            {
+                s += slice[i];
+            }
+            return s;
+        }
+        public static float2 sum(NativeSlice<float2> slice)
+        {
+            float2 s = default;
+            for (int i = 0; i < slice.Length; i++)
+            {
+                s += slice[i];
+            }
+            return s;
+        }
+        public static float3 sum(NativeSlice<float3> slice)
+        {
+            float3 s = default;
+            for (int i = 0; i < slice.Length; i++)
+            {
+                s += slice[i];
+            }
+            return s;
+        }
+        public static int sum(NativeSlice<int> slice)
+        {
+            int s = 0;
+            for (int i = 0; i < slice.Length; i++)
+            {
+                s += slice[i];
+            }
+            return s;
+        }
+        public static int2 sum(NativeSlice<int2> slice)
+        {
+            int2 s = 0;
+            for (int i = 0; i < slice.Length; i++)
+            {
+                s += slice[i];
+            }
+            return s;
+        }
+        public static int3 sum(NativeSlice<int3> slice)
+        {
+            int3 s = 0;
+            for (int i = 0; i < slice.Length; i++)
+            {
+                s += slice[i];
+            }
+            return s;
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
