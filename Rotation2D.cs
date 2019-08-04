ebcb1782f9038fa52ab1bceaf9272aac8b07c44b
JPrzemieniecki <jacek@przemieniecki.net>
2019-08-04 13:38:37 +0200

    Heading2D to quaternion

---
+++ Rotation2D.cs
@@ -0,0 +1,21 @@
+﻿using Unity.Mathematics;
+using UnityEngine;
+using static Unity.Mathematics.math;
+
+namespace JPL
+{
+    public static class Rotation2D
+    {
+        // Equivalent to Quaternion.FromToRotation(left, (heading.x, 0, heading.y))
+        // except this doesn't blow up when heading ~= left and uses no transcendentals
+        public static quaternion HeadingToQuaternion(float2 heading)
+        {
+            if (heading.y < -0.99f)
+            {
+                return quaternion(0, 1, 0, 0);
+            }
+            var norm = sqrt(2 + 2 * heading.y);
+            return quaternion(0, heading.x / norm, 0, norm * 0.5f);
+        }
+    }
+}
